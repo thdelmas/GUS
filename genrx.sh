2da97ec99216f1da95988c5ea72d4bb6acf64a7e
bocal <bocal@car16s6.42barcelona.com. 6.16.11.10.in-addr.arpa name = bocal-6.42barcelona.com>
2022-07-11 21:35:43 +0200

    add: entrypoint

---
+++ genrx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+EMAILS='
+- thdelmas@thdelmas.dev
+'
+WELCOME_MESSAGE="
+Welcome to GenRX
+
+Don't forget to read the documentation
+Don't to hesitate to reach out for any help:
+$EMAILS"
+
+echo "$WELCOME_MESSAGE"
