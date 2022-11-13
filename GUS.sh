96b94ac20077835f3a3b30f537ca90339016e927
Mia <mia@Mia.local>
2022-11-13 15:53:37 +0100

    var mode

--- GUS.sh
+++ GUS.sh
@@ -4,14 +4,20 @@ printf '\n⚙️  Welcome to G.U.S. ! 🚀\n\n'
 
 source gus.env
 
-echo "GUS_METHOD: $GUS_METHOD"
+#
+# Place Holder choose OS Base 
+#
+
+echo 'Launching '"$GUS_METHOD"' mode'
 if [ "$GUS_METHOD" == 'VM' ]
 then
 	./GUS/GUS_vm.sh
 elif [ "$GUS_METHOD" == 'Docker' ]
 then
 	echo "	Not yet implemented"
+	exit 1
 	./GUS/GUS_docker.sh
 else
-	echo "	Undefined"
+	echo "Undefined"
+	exit 1
 fi
--- GUS/GUS_vm.sh
+++ GUS/GUS_vm.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+download_iso () {
+	if ! [ "$1" ]
+	then
+		exit 1
+	fi
+	echo "Downloading from $1"
+	curl -SL --progress-bar -o "./xubuntu.zip" "$1"
+	unzip xubuntu.zip
+	mv Xubuntu_20.04.1_VB_LinuxVMImages.COM.ova "$GUS_VM_ISO"
+}
+
 if ! [ -e "$GUS_VM_ISO" ]
 then
 	echo "ISO not found"
+	download_iso "$GUS_ISO_URL"
 fi
 
 if ! which VBoxManage
@@ -11,4 +23,4 @@ then
 	exit 1
 fi
 
-./VBox/createvm.sh
+./VBox/createvm.42sh
